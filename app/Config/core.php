00c91ad92e02ae711afaf67f276ff244d890eb50
ishigetani <k.subaru1230@gmail.com>
2013-05-23 13:12:50 +0900

    cake設定完了
    DB関連はスカ等で問い合わせてください

--- app/Config/core.php
+++ app/Config/core.php
@@ -186,12 +186,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG93fwrnfowiWRFwmnpi13rmpfo223rdD');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '8495723874129843094802933241134');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
