d2c020203679280c5e4fa95748a5131e518a26c9
ykykyk <n1220092@PC1220092.stcn.iwasaki.ac.jp>
2013-09-24 11:45:41 +0900

    色背景

--- app/webroot/js/cback.js
+++ app/webroot/js/cback.js
@@ -1,4 +1,10 @@
-function color() {
+function color1() {
+	var box = document.getElementById('t1');
+	var color = box.value;
+	box.style.backgroundColor = color;
+	box.style.color = '#FFFFFF';
+}
+function color2() {
 	var box = document.getElementById('t2');
 	var color = box.value;
 	box.style.backgroundColor = color;
