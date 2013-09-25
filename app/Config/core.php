5c8560ecab08821ccf83f37ef28af06f013c1092
ishigetani <s.subaru1230@gmail.com>
2013-09-24 19:03:09 -0700

    color name change

--- app/Config/core.php
+++ app/Config/core.php
@@ -32,9 +32,9 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 0); // 本番用
+//	Configure::write('debug', 0); // 本番用
 //	Configure::write('debug', 1); // pr, debug
-//	Configure::write('debug', 2); // SQL, pr, debug
+	Configure::write('debug', 2); // SQL, pr, debug
 
 /**
  * Configure the Error handler used to handle errors for your application.  By default
